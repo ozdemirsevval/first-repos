be1487bef60c332610333f6333061cd671cf9e20
ozdemirsevval <sevvalozdemir2002@windowslive.com>
2023-11-03 16:16:40 +0300

    giriş yazısı Tofi tarafından değiştirildi.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archi's Academy GitHub Kursu 
+          Git & GitHub Öğreniyorum w/Tofi 
         </p>
         <a
           className="App-link"
